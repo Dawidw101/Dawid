6448cb83212ff5f3461238f5c8b1451ee053626b
Dawidw101 <76258206+Dawidw101@users.noreply.github.com>
2020-12-29 13:52:18 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-const btn   = document.querySelector("#btn");
-const list  = document.querySelector("#list");
-const el    = document.getElementsByTagName("li");
+const btn = document.querySelector("#btn");
+const list = document.querySelector("#list");
+const el = document.getElementsByTagName("li");
 const input = document.querySelector("#input");
-const focusInput = function() {
-    input.focus();
-  };
-input.addEventListener("keyup", function(event) {
-    
-    if (event.keyCode === 13) { 
-        
-        event.preventDefault();// Standardowy zapis - wytlumacze pozniej 
-        // wywolaj funkcje tak jakby ktos zrobil click na ptzycisku
-        btn.click();
-      }
+
+const focusInput = function () {
+  input.focus();
+};
+const zaznaczzrobione = function () {
+  //v.parentNode.classList.toggle("checked");
+  console.log("jestem zaznaczzrobione");
+  console.log(this.parentNode.classList.toggle("checked"));
+  //console.log(v.parentNode);
+};
+const zaznaczzrobione1 = function () {
+  //v.parentNode.classList.toggle("checked");
+  console.log("jestem zaznaczzrobione1");
+  this.preventDefault();
+  console.log(this.classList.toggle("checked"));
+  //console.log(v.parentNode);
+};
+
+input.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault(); // Standardowy zapis - wytlumacze pozniej
+    // wywolaj funkcje tak jakby ktos zrobil click na ptzycisku
+    btn.click();
+  }
 });
 
 btn.onclick = function () {
@@ -21,40 +34,55 @@ btn.onclick = function () {
     alert("Musisz wpisać zadanie");
   } else {
     li = document.createElement("li");
-    li.innerHTML = '<div onclick = "zaznaczzrobione(this)">'+txt+'</div>';
+    // li.innerHTML = '<div onclick = "zaznaczzrobione(this)">'+txt+'</div>';
+    li.onclick = zaznaczzrobione1;
+    let div = document.createElement("div");
+    div.onclick = zaznaczzrobione;
+    div.className = "selection";
+    div.innerText = txt;
+
+    li.appendChild(div);
     list.appendChild(li);
     input.value = "";
-	
     let deleteButton = document.createElement("button");
+
+    document.querySelectorAll(".selection").onclick = function (event) {
+      console.log(event);
+      console.log("loistoncliktriggerd");
+      // if (v.target.tagName == "LI") {
+      event.target.classList.toggle("checked");
+      //}
+    };
+
     deleteButton.innerText = "x";
+
     deleteButton.onclick = function (del) {
-      if(confirm("Are you sure you want to delete task?")){
-		  
-          let item = del.target.parentNode;
-          item.remove();//tu nalezy wpisac funkcje do usuwania zadania
+      alert(del);
+      console.log(del);
+      console.log(this);
+
+      if (confirm("Are you sure you want to delete task?")) {
+        let item = del.target.parentNode;
+        //item = this.parentNode;
+        item.remove(); //tu nalezy wpisac funkcje do usuwania zadania
       }
-      focusInput();//tu tez musimy ustalic focus
+      focusInput(); //tu tez musimy ustalic focus
     };
     li.appendChild(deleteButton);
-    focusInput();//po delete tez focus
+    focusInput(); //po delete tez focus
   }
 };
-
-
-list.onclick = function (ev) {
-  if (ev.target.tagName == "LI") {
-    ev.target.classList.toggle("checked");
-  }
+/*
+document.querySelectorAll(".selection").onclick = function (event) {
+  console.log(event);
+  console.log("loistoncliktriggerd");
+  // if (v.target.tagName == "LI") {
+  event.target.classList.toggle("checked");
+  //}
 };
-
-const zaznaczzrobione = function(v)
-{
-	v.parentNode.classList.toggle("checked");
-	console.log(v)
-console.log(v.parentNode);
-}
+*/
 
 window.onload = function () {
-    //tu wykonują się rzeczy dopiero po PEŁNYM załadowaniu skryptu JS
-    focusInput();
+  //tu wykonują się rzeczy dopiero po PEŁNYM załadowaniu skryptu JS
+  focusInput();
 };
